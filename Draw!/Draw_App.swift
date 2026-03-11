e7afb9802fb0351aca1c897e7498338fbb9a3543
Trungkun103 <trungkun103@gmail.com>
2026-03-11 17:58:20 +0700

    Initial Commit

---
+++ Draw!/Draw_App.swift
@@ -0,0 +1,17 @@
+//
+//  Draw_App.swift
+//  Draw!
+//
+//  Created by TrungAnhx on 11/3/26.
+//
+
+import SwiftUI
+
+@main
+struct Draw_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
